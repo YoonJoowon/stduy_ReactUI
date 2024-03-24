97ea6361a678783b32992d6efde3343ba181939a
Sleepingoff <justjh30@gmail.com>
2024-03-24 22:20:15 +0900

    경로 alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: "src/components", replacement: "@/components" },
+      { find: "src/types", replacement: "@/types" },
+      { find: "src/utils", replacement: "@/utils" },
+      { find: "src/hooks", replacement: "@/hooks" },
+    ],
+  },
+});
