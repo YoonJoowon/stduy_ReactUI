44e28f8be6da872ded0007011251078797480810
yoonjoowon <wndnjs6224@naver.com>
2024-03-27 11:51:19 +0900

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,7 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-=======
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
->>>>>>> f43d1ad558962532cb6ae53b0abd24890f71966e
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
 })
-=======
-  resolve: {
-    alias: [
-      { find: "src/components", replacement: "@/components" },
-      { find: "src/types", replacement: "@/types" },
-      { find: "src/utils", replacement: "@/utils" },
-      { find: "src/hooks", replacement: "@/hooks" },
-    ],
-  },
-});
->>>>>>> f43d1ad558962532cb6ae53b0abd24890f71966e
